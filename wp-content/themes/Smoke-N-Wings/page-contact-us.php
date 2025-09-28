2f9677ad587864def871fc117ff86233aaa4f403
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2025-09-28 13:40:10 +0600

    daynamic contact page commit

--- wp-content/themes/Smoke-N-Wings/page-contact-us.php
+++ wp-content/themes/Smoke-N-Wings/page-contact-us.php
@@ -1,10 +1,8 @@
 <?php get_header();?>
 
 <?php get_template_part('sections/breadcrumb_page'); ?>
+<?php get_template_part('sections/contact_form'); ?>
 
-<div class="max-w-lg mx-auto p-6 bg-white shadow-lg rounded-lg">
-    <?php echo do_shortcode('[contact-form-7 id="7f08c2e" title="Contact Us"]'); ?>
-</div>
 
 
 
